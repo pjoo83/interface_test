b5a01813f98cdd188b043fd98eb2e4fbf23a2c9a
pjoo83 <pjoo83@163.com>
2024-03-18 11:25:22 +0800

    新增主页功能

--- myapp/views/index.py
+++ myapp/views/index.py
@@ -1,5 +1,36 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def translate_bar(request):
+    legend = ['fuck', 'curry']
+    data_list = [
+        {
+            "name": 'fuck',
+            "type": 'bar',
+            "data": [1, 20, 36, 10, 10, 90, 1, 20, 36, 10, 10, 90, 1, 20, 36, 10, 10, 90]
+        },
+        {
+            "name": 'curry',
+            "type": 'bar',
+            "data": [1, 20, 36, 10, 10, 90, 1, 20, 36, 10, 10, 90,1, 20, 36, 10, 10, 90]
+        }
+    ]
+    date_list = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '一月', '二月', '三月', '四月', '五月', '六月',
+                 '七月', '五月', '六月',
+                 '七月', '五月', '六月',
+                 '七月']
+
+    result = {
+        "status": True,
+        "data": {
+            "legend": legend,
+            "data_list": data_list,
+            "date_list": date_list
+        }
+    }
+    return JsonResponse(result)

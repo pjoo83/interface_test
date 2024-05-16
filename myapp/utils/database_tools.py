f0381e34076690a0025257b574386cf66423c6c4
pjoo83 <pjoo83@163.com>
2024-05-16 17:01:04 +0800

    完成这线条

--- myapp/utils/database_tools.py
+++ myapp/utils/database_tools.py
@@ -1,24 +1,50 @@
 import pymysql
 
 
-def execute_sql(channel_id, content):
+def execute_sql(sid, channel_id, content):
     db = pymysql.connect(
         host='127.0.0.1',
         user='root',
         password='zxcv1234',
         database='localhost_interface'
     )
-    filter_execute_statistics = f"select {content} from  translate_statistics where channel_id ={channel_id} order by time desc;"
-    cursor = db.cursor()
-    try:
-        cursor.execute(filter_execute_statistics)
-        result = cursor.fetchall()
-        return result
-    except pymysql.MySQLError as e:
-        print(e, '查询失败')
-    db.commit()
-    db.close()
+    if sid == 1:
+        filter_execute_statistics = f"select {content} from  translate_statistics where channel_id ={channel_id} order by time desc limit 5;"
+        cursor = db.cursor()
+        try:
+            cursor.execute(filter_execute_statistics)
+            result = cursor.fetchall()
+            return result
+        except pymysql.MySQLError as e:
+            print(e, '查询失败')
+        db.commit()
+        db.close()
+    elif sid == 2:
+        filter_execute_statistics = f'SELECT  CAST(time AS DATE) AS date_only, COUNT(*)  FROM translate_statistics  GROUP BY date_only ORDER BY date_only DESC limit 5;'
+        cursor = db.cursor()
+        try:
+            cursor.execute(filter_execute_statistics)
+            result = cursor.fetchall()
+            return result
+        except pymysql.MySQLError as e:
+            print(e, '查询失败')
+        db.commit()
+        db.close()
+
+
+def package_execute(new_list, sid, cid, statement):
+    """
+    :param statement: sql语句
+    :param cid:端id
+    :param sid:执行的语句id
+    :param new_list:导入表格
+    :return:数据表
+    """
+    new = execute_sql(sid, cid, statement)
+    for i in new:
+        new_list.append(i[0])
+    return new_list
 
 
 if __name__ == '__main__':
-    print(execute_sql(2, 'quantity'))
+    print(execute_sql(2, 2, 'quantity'))
--- myapp/views/dashboard_view.py
+++ myapp/views/dashboard_view.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from ..utils.database_tools import execute_sql
+from myapp.utils.database_tools import execute_sql, package_execute
 
 
 def dashboard_index(request):
@@ -8,48 +8,84 @@ def dashboard_index(request):
 
 
 def dashboard_translate_bar(request):
-    legend = ['新增', '修改']
-    new_list = []
-    new = execute_sql(2, 'newly_quantity')
-    for i in new:
-        new_list.append(i[0])
-    modify_list = []
-    modify = execute_sql(2, 'modify_quantity')
-    for i in modify:
-        modify_list.append(i[0])
-    data_list = [
+    android_new_list = []
+    package_execute(android_new_list, 1, 1, 'newly_quantity')
+    android_modify_list = []
+    package_execute(android_modify_list, 1, 1, 'modify_quantity')
+    ios_new_list =[]
+    package_execute(ios_new_list, 1, 2, 'newly_quantity')
+    ios_modify_list=[]
+    package_execute(ios_modify_list, 1, 2, 'modify_quantity')
+    server_new_list = []
+    package_execute(server_new_list, 1, 3, 'newly_quantity')
+    series = [
         {
-            "name": 'fuck',
-            "type": 'bar',
-            "data": new_list
+            'name': 'android新增',
+            'type': 'bar',
+            'stack': 'Aa',
+            'emphasis': {
+                'focus': 'series'
+            },
+            'data': android_new_list[::-1]
+        }, {
+            'name': 'android修改',
+            'type': 'bar',
+            'stack': 'Aa',
+            'emphasis': {
+                'focus': 'series'
+            },
+            'data': android_modify_list[::-1]
         },
         {
-            "name": 'curry',
-            "type": 'bar',
-            "data": modify_list
-        }
+            'name': 'ios新增',
+            'type': 'bar',
+            'stack': 'Ab',
+            'emphasis': {
+                'focus': 'series'
+            },
+            'data': ios_new_list[::-1]
+        },
+        {
+            'name': 'ios修改',
+            'type': 'bar',
+            'stack': 'Ab',
+            'emphasis': {
+                'focus': 'series'
+            },
+            'data': ios_modify_list[::-1]
+        },
+        {
+            'name': 'server新增',
+            'type': 'bar',
+            'stack': 'Ac',
+            'emphasis': {
+                'focus': 'series'
+            },
+            'data': server_new_list[::-1]
+        },
     ]
-    date_list = ['一月', '二月', '三月', '四月', '五月', '六月']
-
+    date_list = []
+    package_execute(date_list, 2, 2, 'quantity')
     result = {
         "status": True,
         "data": {
-            "legend": legend,
-            "data_list": data_list,
+            # "legend": legend,
+            "data_list": series,
             "date_list": date_list
         }
     }
+    print(series)
     return JsonResponse(result)
 
 
 def dashboard_language_count(request):
     """
-    :param request:
+    :param request: 请求表头总多语言数量
     :return: 返回值
     """
-    android_count = execute_sql(1, 'quantity')[0]
-    ios_count = execute_sql(2, 'quantity')[0]
-    server_count = execute_sql(3, 'quantity')[0]
+    android_count = execute_sql(1, 1, 'quantity')[0]
+    ios_count = execute_sql(1, 2, 'quantity')[0]
+    server_count = execute_sql(1, 3, 'quantity')[0]
     # unity_count = execute_sql(4, 'quantity')[0]
     data = {
         'android': android_count,
@@ -70,7 +106,7 @@ def dashboard_executions_total(request):
     :param request:
     :return: 返回脚本执行次数
     """
-    total = len(execute_sql(2, 'quantity'))
+    total = len(execute_sql(1, 2, 'quantity'))
     data = {
         "status": True,
         'data': total
@@ -80,12 +116,12 @@ def dashboard_executions_total(request):
 
 def dashboard_translate_pie(request):
     """
-    :param request:
+    :param request: 饼图数据
     :return: 返回各项总数
     """
-    android = execute_sql(1, "quantity")[0][0]
-    ios = execute_sql(2, "quantity")[0][0]
-    server = execute_sql(3, "quantity")[0][0]
+    android = execute_sql(1, 1, "quantity")[0][0]
+    ios = execute_sql(1, 2, "quantity")[0][0]
+    server = execute_sql(1, 3, "quantity")[0][0]
     data = [
         {'value': android, 'name': '安卓'},
         {'value': ios, 'name': 'ios'},
@@ -98,5 +134,4 @@ def dashboard_translate_pie(request):
         "data": {
             "data_list": data}
     }
-    print(result)
     return JsonResponse(result)

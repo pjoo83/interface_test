58fd7ab9ea6f527e68696769c564c016c044ab9d
pjoo83 <pjoo83@163.com>
2024-06-19 18:37:58 +0800

    中间键修改

--- Interface_Test/settings.py
+++ Interface_Test/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'myapp.middleware.auth.AuthMiddleWare'
+    'myapp.middleware.auth.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'Interface_Test.urls'

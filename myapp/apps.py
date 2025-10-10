1fd04fdd4f594d006e3d90b7f63c9b7d5658a85d
pjoo83 <pjoo83@163.com>
2025-10-10 10:14:23 +0800

    增加定时任务自动刷新token

--- myapp/apps.py
+++ myapp/apps.py
@@ -3,12 +3,25 @@ from apscheduler.triggers.cron import CronTrigger
 from django.apps import AppConfig
 
 from myapp.utils.time import call_api_task
+from myapp.utils.feishu_get_token import get_plugin_access_token
+
+_plugin_token_cache = {"token": None}
+
+
+def refresh_feishu_token():
+    print("刷新飞书 Token ...")
+    _plugin_token_cache["token"] = get_plugin_access_token()
 
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
 
+    def ready(self):
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(refresh_feishu_token, 'interval', minutes=115)
+        scheduler.start()
+        print("Token 自动刷新任务已启动")
     # def ready(self):
     #     scheduler = BackgroundScheduler()
     #     # # 每两小时的第0分钟执行
